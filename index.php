5e43c0452500206ff38bf8223714e1ac204c0970
Arya Pujianto <aryapujianto@inarts.co.id>
2017-12-09 06:45:06 +0700

    oke

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo getenv('MYSQL_DATABASE')."</br>";
+echo getenv('MYSQL_USER')."</br>";
+echo getenv('MYSQL_ROOT_PASSWORD')."</br>";
+
+echo getenv('OPENSHIFT_MYSQL_DB_HOST')."</br>";
+echo getenv('OPENSHIFT_MYSQL_DB_PORT')."</br>";
+echo getenv('OPENSHIFT_MYSQL_DB_USERNAME')."</br>";
