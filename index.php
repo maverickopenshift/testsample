b1dd3c93f05018f24318b80dd949be1f6ba28b56
Arya Pujianto <aryapujianto@inarts.co.id>
2017-12-09 12:39:10 +0700

    oke

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
-echo getenv('MYSQL_DATABASE')."</br>";
-echo getenv('MYSQL_USER')."</br>";
-echo getenv('MYSQL_ROOT_PASSWORD')."</br>";
+$mysqli = mysqli_connect(
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    getenv('OPENSHIFT_MYSQL_DB_PORT')
+);
 
-echo getenv('OPENSHIFT_MYSQL_DB_HOST')."</br>";
-echo getenv('OPENSHIFT_MYSQL_DB_PORT')."</br>";
-echo getenv('OPENSHIFT_MYSQL_DB_USERNAME')."</br>";
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
