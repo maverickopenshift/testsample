f7e497f87a3006de1e5583102e1eab37c92cd8f4
Arya Pujianto <aryapujianto@inarts.co.id>
2017-12-09 13:12:38 +0700

    oke

--- index.php
+++ index.php
@@ -8,14 +8,13 @@ $mysqli = mysqli_connect(
     getenv('MARIADBCONSYS_SERVICE_PORT')
 );
 
-if (!$link) {
+if (!$mysqli) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($mysqli) . PHP_EOL;
 
-mysqli_close($link);
+mysqli_close($mysqli);
