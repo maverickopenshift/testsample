3c3ea39a362af3c1d545b916c4bf8249690063a4
Arya Pujianto <aryapujianto@inarts.co.id>
2017-12-09 16:07:33 +0700

    oke

--- index.php
+++ index.php
@@ -17,4 +17,14 @@ if (!$mysqli) {
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($mysqli) . PHP_EOL;
 
+$res = $mysqli->query("SELECT * FROM pegawai ORDER BY id ASC LIMIT 10");
+
+echo "Reverse order...\n";
+for ($row_no = $res->num_rows - 1; $row_no >= 0; $row_no--) {
+    $res->data_seek($row_no);
+    $row = $res->fetch_assoc();
+    echo " id = " . $row['id'] . "<br>";
+    echo " nama = " . $row['v_nama_karyawan'] . "<br>";
+}
+
 mysqli_close($mysqli);
